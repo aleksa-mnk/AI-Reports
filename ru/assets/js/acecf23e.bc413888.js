33da9b63a4ce51ec4b2cd2076e7d9f507274e05b
aleksa-mnk <aleksamnk@outlook.com>
2024-09-19 10:31:03 +0300

    Deploy website - based on d20c6639eeba0bb35eef70b91fac9ddb76952ee1

---
+++ ru/assets/js/acecf23e.bc413888.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/my-website/build/index.html/ru/blog","blogTitle":"Blog","authorsListPath":"/my-website/build/index.html/ru/blog/authors"}')}}]);
---
+++ ru/assets/js/f32ad9e8.aacf9a7a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2119],{5029:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/my-website/build/index.html/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
