a549b26feeb7f5203be4da9086388665c1462eb3
aleksa-mnk <aleksamnk@outlook.com>
2024-09-19 10:25:52 +0300

    Deploy website - based on d20c6639eeba0bb35eef70b91fac9ddb76952ee1

---
+++ ru/assets/js/89b632da.f522cd89.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7052],{3175:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/AI-Reports/ru/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ ru/assets/js/acecf23e.091effd1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/AI-Reports/ru/blog","blogTitle":"Blog","authorsListPath":"/AI-Reports/ru/blog/authors"}')}}]);
